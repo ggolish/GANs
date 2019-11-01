b6ce58cd39818d808288ba614aac2923e4022d2e
Adam Davenport <adavenport9@sycamores.indstate.edu>
2019-11-01 13:55:15 -0400

    Cleaning up image downloading/loading

--- loader/ross.py
+++ loader/ross.py
@@ -16,7 +16,7 @@ data_url = "https://cs.indstate.edu/~ggolish/data/ross-data-{}.tar.gz"
 data_dest = "/tmp/ross-data-{}.tar.gz"
 final_dir = "/tmp/ross-data-{}"
 
-def download_ross_data(verbose=False):
+def download_ross_data(verbose=True):
     if verbose:
         print("Retrieving Bob Ross data set...")
 
@@ -32,33 +32,5 @@ def download_ross_data(verbose=False):
     if verbose:
         print("Done.")
 
-def load_data(version="resized", verbose=False):
-    global data_url, data_dest, final_dir
-    data_url = data_url.format(version)
-    data_dest = data_dest.format(version)
-    final_dir = final_dir.format(version)
-
-    if not os.path.exists(final_dir):
-        download_ross_data(verbose=verbose)
-
-    if verbose:
-        print("Reading Bob Ross data set...")
-
-    files = [f for f in os.listdir(final_dir) if f.endswith(".png")]
-    images = []
-    for f in tqdm(files):
-        if f.endswith(".png"):
-            path = os.path.join(final_dir, f)
-            img = imageio.imread(path)
-            images.append(img)
-
-    if verbose:
-        print("Done.")
-
-    return np.array(images, dtype="float32")
-
-
 if __name__ == "__main__":
-
-    images = load_data(verbose=True)
-    print("Data set shape:", images.shape)
+    download_ross_data(verbose=True)
