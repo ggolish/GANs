02a64898538cff8c80da7df624ade595a1d7650d
Adam Davenport <adam@galacticweapons.com>
2019-11-02 17:02:21 -0400

    Making downloader more moduler

--- loader/ross.py
+++ loader/ross.py
@@ -4,33 +4,12 @@
     Module for loading the Bob Ross data set as a numpy array
 '''
 
-import imageio
-import numpy as np
-import os
-import tarfile
+import downloader
 
-from urllib.request import urlretrieve
-from tqdm import tqdm
+ds = 'ros-data-resized'
+data_url = f'http://cs.indstate.edu/~adavenport9/data/wikiart/{ds}.tar.gz'
+data_dest = f'/tmp/{ds}.tar.gz'
+final_dir = f'/tmp/{ds}'
 
-data_url = "https://cs.indstate.edu/~ggolish/data/ross-data-resized.tar.gz"
-data_dest = "/tmp/ross-data-resized.tar.gz"
-final_dir = "/tmp/ross-data-resized"
-
-def download_ross_data(verbose=True):
-    if verbose:
-        print("Retrieving Bob Ross data set...")
-
-    urlretrieve(data_url, filename=data_dest)
-
-    if verbose:
-        print("Done.")
-        print("Extracting Bob Ross data set...")
-
-    with tarfile.open(data_dest, "r") as tfd:
-        tfd.extractall("/tmp")
-
-    if verbose:
-        print("Done.")
-
-if __name__ == "__main__":
-    download_ross_data(verbose=True)
+def download():
+    downloader.download_data(data_url, data_dest)
