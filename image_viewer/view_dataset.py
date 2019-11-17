37a517483e1cb8bd1ae9da2a2bbc49815b4b3b70
Adam Davenport <adavenport9@sycamores.indstate.edu>
2019-11-16 20:15:30 -0500

    Creating image viewer module

---
+++ image_viewer/view_dataset.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+from random import shuffle
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+from loader import cubism
+
+
+def show_images(imgs: np.array, cols, rows, shuffle=True):
+    shuffle(imgs)
+    fig=plt.figure(figsize=(8, 8))
+    for i in range(1, cols*rows +1):
+        fig.add_subplot(rows, columns, i)
+        # Have to do i-1 due to the indexes here
+        plt.imshow(imgs[i-1])
+    plt.show()
+
+if __name__ == '__main__':
+    imgs = cubism.load_np
+    show_images(imgs, 10, 10)
