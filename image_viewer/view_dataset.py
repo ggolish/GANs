19bc0c4e9a5f0b15cee8077b9f58425d089a1af2
Adam <adavenport9@sycamores.indstate.edu>
2019-11-17 11:00:41 -0500

    Formatting

--- image_viewer/view_dataset.py
+++ image_viewer/view_dataset.py
@@ -10,16 +10,18 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 from loader import cubism
 
 
-def show_images(imgs: np.array, cols, rows, shuffle=True):
+def show_images(imgs: np.array, cols, rows, randomize=False):
     """ Take images stored in a np array and plot them. """
-    shuffle(imgs)
-    fig=plt.figure(figsize=(8, 8))
-    for i in range(1, cols*rows +1):
+    if randomize:
+        shuffle(imgs)
+    fig = plt.figure(figsize=(8, 8))
+    for i in range(1, cols*rows + 1):
         fig.add_subplot(rows, cols, i)
         # Have to do i-1 due to the indexes here
         plt.imshow(imgs[i-1])
     plt.show()
 
+
 if __name__ == '__main__':
-    imgs = cubism.load_np
+    imgs = cubism.load_np()
     show_images(imgs, 10, 10)
--- loader/loader.py
+++ loader/loader.py
@@ -4,13 +4,6 @@
     Module for loading an image data set as a numpy array
 """
 
-if __name__ == "loader.loader":
-    from . import img_utils
-    from .downloader import download
-else:
-    import img_utils
-    from downloader import download
-
 import cv2
 import numpy as np
 import os
@@ -18,6 +11,13 @@ import sys
 import argparse
 import torch
 from tqdm import tqdm
+
+if __name__ == "loader.loader":
+    from . import img_utils
+    from .downloader import download
+else:
+    import img_utils
+    from downloader import download
 
 
 def create_loader(data, batch_size=128):
@@ -27,24 +27,24 @@ def create_loader(data, batch_size=128):
 
 def load_data(ds_info, optimize=True, verbose=False, imsize=256):
 
-    if not os.path.exists(ds_info["data_dest"]):
+    if not os.path.exists(ds_info['data_dest']):
         download(ds_info)
 
     if verbose:
-        print("Reading image data set...")
+        print('Reading image data set...')
 
     if optimize:
-        ds_info["final_dest"] = f"{ds_info['final_dir']}.{imsize}.optimized.npy"
+        ds_info['final_dest'] = f'{ds_info["final_dir"]}.{imsize}.optimized.npy'
     else:
-        ds_info["final_dest"] = f"{ds_info['final_dir']}.{imsize}.npy"
+        ds_info['final_dest'] = f'{ds_info["final_dir"]}.{imsize}.npy'
 
     ds_info['final_dest'] = ds_info['final_dest'].format(imsize)
 
-    if not os.path.exists(ds_info["final_dest"]):
-        files = [f for f in os.listdir(ds_info["final_dir"]) if f.endswith(".png") or f.endswith(".jpg")]
+    if not os.path.exists(ds_info['final_dest']):
+        files = [f for f in os.listdir(ds_info['final_dir']) if f.endswith('.png') or f.endswith('.jpg')]
         images = list()
         for f in tqdm(files, ascii=True):
-            path = os.path.join(ds_info["final_dir"], f)
+            path = os.path.join(ds_info['final_dir'], f)
             original = cv2.imread(path)
             img = cv2.resize(original, dsize=(imsize, imsize), interpolation=cv2.INTER_CUBIC)
             images.append(img)
@@ -54,34 +54,32 @@ def load_data(ds_info, optimize=True, verbose=False, imsize=256):
                 images.append(img_utils.southwest(original, imsize))
                 images.append(img_utils.southeast(original, imsize))
                 images.append(img_utils.northeast(original, imsize))
-        imgnpy = np.array(images, dtype="float32")
+        imgnpy = np.array(images, dtype='float32')
 
         # Get the data ready for a pytorch GAN
         imgnpy = imgnpy / 127.5 - 1.0
         n, _, _, channels = imgnpy.shape
         imgnpy = imgnpy.reshape(n, channels, imsize, imsize)
-        np.save(ds_info["final_dest"], imgnpy)
+        np.save(ds_info['final_dest'], imgnpy)
 
     else:
-        imgnpy = np.load(ds_info["final_dest"])
+        imgnpy = np.load(ds_info['final_dest'])
 
     if verbose:
-        print("Done.")
+        print('Done.')
 
     return imgnpy
 
 
 if __name__ == "__main__":
     import ross
-    import cubism
-    parser = argparse.ArgumentParser(prog="image-loader", description="Utility for loading images")
-    parser.add_argument("-s", "--size", help="Set the size of the images.")
+    parser = argparse.ArgumentParser(prog='image-loader', description='Utility for loading images')
+    parser.add_argument('-s', '--size', help='Set the size of the images.')
     args = parser.parse_args(sys.argv[1:])
     if args.size:
         try:
             imsize = int(args.size)
         except ValueError:
             sys.stderr.write('Image size must be an integer.\n')
-   
     print('Testing ross data set')
     print(ross.load())
