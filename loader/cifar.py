613604e19b6942ee13b0b65748b3c72a823eed08
ggolish <ggolish@sycamores.indstate.edu>
2019-11-21 14:57:46 -0500

    Made function conform to other ds load functions.

--- loader/cifar.py
+++ loader/cifar.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import torch
 import numpy as np
 import torchvision
@@ -18,12 +19,14 @@ classes = {
     "truck": 9,
 }
 
-def load(batch_size=128, class_type="cat"):
+def load(class_type="cat", optimize=True, imsize=32, batch_size=128, verbose=True):
     global classes
+    if imsize != 32:
+        sys.stderr.write("Warning: CIFAR10 dataset only compatible with imsize 32!\n")
     ds = torchvision.datasets.CIFAR10("/tmp", train=False, download=True)
     indeces = [i for i in range(len(ds)) if ds[i][1] == classes[class_type]]
-    imgs = np.transpose((ds.data[indeces] - 127.5) / 127.5, (0, 3, 1, 2))
-    return DataLoader(imgs, batch_size=batch_size, shuffle=True)
+    imgs = ((ds.data[indeces] - 127.5) / 127.5).reshape(1000, 3, 32, 32)
+    return DataLoader(imgs.astype("float32"), batch_size=batch_size, shuffle=True)
 
 if __name__ == "__main__":
     dl = load()
