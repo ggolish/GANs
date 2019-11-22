9656700cec5e6b94232c33b5c0716651bffe92e9
ggolish <ggolish@sycamores.indstate.edu>
2019-11-22 14:43:50 -0500

    Added loading function to trainer module.

--- trainer.py
+++ trainer.py
@@ -23,14 +23,37 @@ def store_results(results: dict, gan: artgan.GAN, name: str, dest:str="results")
     dest = os.path.join(dest, name)
     gan_dest = os.path.join(dest, "gan.pt")
     results_dest = os.path.join(dest, "results.pickle")
+    settings_dest = os.path.join(dest, "settings.pickle")
     os.makedirs(dest, exist_ok=True)
 
-     
     torch.save({
         'generator_state_dict': gan.G.arch.state_dict(),
         'critic_state_dict': gan.D.arch.state_dict()
     }, gan_dest)
 
+    with open(settings_dest, "wb") as fd:
+        pickle.dump(gan.S, fd)
+
     with open(results_dest, "wb") as fd:
         pickle.dump(results, fd)
+    
+def load_results(name: str, dest:str="results"):
+    dest = os.path.join(dest, name)
+    gan_dest = os.path.join(dest, "gan.pt")
+    results_dest = os.path.join(dest, "results.pickle")
+    settings_dest = os.path.join(dest, "settings.pickle")
+
+    with open(settings_dest, "rb") as fd:
+        settings = pickle.load(fd)
+
+    with open(results_dest, "rb") as fd:
+        results = pickle.load(fd)
+
+    gan = artgan.GAN(settings)
+    pt = torch.load(gan_dest)
+    gan.D.arch.load_state_dict(pt["critic_state_dict"])
+    gan.G.arch.load_state_dict(pt["generator_state_dict"])
+
+    return (results, gan)
+
     
