23dd366b1c3d1fcef782e8c10ab859d2253d4aac
Adam <adavenport9@sycamores.indstate.edu>
2019-11-22 22:37:09 -0500

    Begingin dataset refactor

--- loader/cubism.py
+++ loader/cubism.py
@@ -17,19 +17,24 @@ ds = 'cubism'
 ds_info = {
     'name': ds,
     'local_dir': f'/u1/h3/adavenport9/public_html/data/{ds}',
-    'data_url': f'http://cs.indstate.edu/~adavenport9/data/wikiart/{ds}.tar.gz',
+    'data_url': f'http://cs.indstate.edu/~adavenport9/data/{ds}.tar.gz',
     'data_dest': f'/tmp/{ds}.tar.gz',
     'final_dir': f'/tmp/{ds}',
     'final_dest': f'/tmp/{ds}.npy'
 }
 
 
-def load(optimize=True, imsize=256, batch_size=128, verbose=True):
+def load(optimize=True, imsize=64, batch_size=128, verbose=True):
     global ds_info
+    # Add the image size to the dataset
+    for k, v in ds_info.items():
+        ds_info[k] = v.replace(ds, f'{ds}.{imsize}')
+    from pprint import pprint
+    pprint(ds_info)
     return load_data(ds_info, optimize=optimize, verbose=True, imsize=imsize, batch_size=batch_size)
 
 
-def load_np(optimize=True, imsize=256, batch_size=128, verbose=True, nbatches=1):
+def load_np(optimize=True, imsize=64, batch_size=128, verbose=True, nbatches=1):
     global ds_info
     dl = load_data(ds_info, optimize=optimize, verbose=True, imsize=imsize)
     li = []
@@ -40,4 +45,3 @@ def load_np(optimize=True, imsize=256, batch_size=128, verbose=True, nbatches=1)
 
 if __name__ == '__main__':
     load(optimize=False)
-    
