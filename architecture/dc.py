ecf19e5d96eb2bfaffa084e23936fb673dc5a54a
ggolish <ggolish@sycamores.indstate.edu>
2019-11-25 02:00:17 -0500

    Reimplemented critic architecture for DCGAN.

--- architecture/dc.py
+++ architecture/dc.py
@@ -1,71 +1,101 @@
 #!/usr/bin/env python3
-"""
-    Differenet architectures to be used as the generator or critic in a WGAN.
-    The architectures are specific to the data being used in our experiments.
-"""
 
-import math
+''' The standard DCGAN architectures for Critic and Generator. '''
+
 import torch
-from torch.nn import Conv2d, ConvTranspose2d, BatchNorm2d, Linear, Module
-from torch.nn.functional import leaky_relu, relu
+import math
 
-if __name__ == "architecture.dc":
-    from .common import Identity
-else:
-    from common import Identity
+from torch.nn import Module, Conv2d, ConvTranspose2d, BatchNorm2d, LeakyReLU, ReLU, Tanh, Sigmoid
 
-class DCGAN(Module):
-    """ Deep Convolutional GAN architecture, for image sizes n s.t. 2^x = n where x is in N^+"""
 
-    def __init__(self, is_critic, S):
+class CriticArchitecture(Module):
+    ''' Standard DCGAN critic architecture (base 2 image sizes only) '''
+
+    def __init__(self, settings: dict, debug: bool = False):
         super().__init__()
-        self.is_critic = is_critic
-        self.nchannels = S['nchannels']
-        self.nfeatures = S['nfeatures']
-        self.zdim = S['zdim']
-        self.gp_enabled = S['gp_enabled']
-        
-        self.final_size = S['image_size']
-        for _ in range(4):
-            self.final_size = convolution_size(self.final_size)
-        self.fc1_size = 8 * self.nfeatures * self.final_size * self.final_size
 
-        if is_critic:
-            self.conv1 = Conv2d(self.nchannels, self.nfeatures, 3, stride=2)
-            self.conv2 = Conv2d(self.nfeatures, self.nfeatures * 2, 3, stride=2)
-            self.bn1 = Identity() if self.gp_enabled else BatchNorm2d(self.nfeatures * 2)
-            self.conv3 = Conv2d(self.nfeatures * 2, self.nfeatures * 4, 3, stride=2)
-            self.bn2 = Identity() if self.gp_enabled else BatchNorm2d(self.nfeatures * 4)
-            self.conv4 = Conv2d(self.nfeatures * 4, self.nfeatures * 8, 3, stride=2)
-            self.bn3 = Identity() if self.gp_enabled else BatchNorm2d(self.nfeatures * 8)
-            self.fc1 = Linear(self.fc1_size, 1)
-        else:
-            self.fc1 = Linear(self.zdim, self.fc1_size)
-            self.conv1 = ConvTranspose2d(8 * self.nfeatures, 4 * self.nfeatures, 3, stride=2)
-            self.bn1 = BatchNorm2d(4 * self.nfeatures)
-            self.conv2 = ConvTranspose2d(4 * self.nfeatures, 2 * self.nfeatures, 3, stride=2)
-            self.bn2 = BatchNorm2d(2 * self.nfeatures)
-            self.conv3 = ConvTranspose2d(2 * self.nfeatures, self.nfeatures, 3, stride=2)
-            self.bn3 = BatchNorm2d(self.nfeatures)
-            self.conv4 = ConvTranspose2d(self.nfeatures, self.nchannels, 4, stride=2)
+        # Store necessary settings from parent GAN
+        self.gp_enabled = settings['gp_enabled']
+        self.imsize = settings['image_size']
+        self.nchannels = settings['nchannels']
+        self.nfeatures = settings['nfeatures']
+        self.debug = debug
+
+        # This architecure will only be for image sizes that are powers of two
+        # (for simplicity)
+        if not isbase2(self.imsize):
+            raise Exception(f"Invalid image size for DCGAN: {self.imsize}")
+
+        # Construct the necessary number of layers
+        self.layers = []
+        self.layers.append(
+            Conv2d(self.nchannels, self.nfeatures, 4, 2, 1, bias=False))
+        self.layers.append(LeakyReLU(0.02, inplace=True))
+        mult = 1
+        for i in range(int(math.log2(self.imsize)) - 3):
+            conv2d = Conv2d(self.nfeatures * mult,
+                            self.nfeatures * mult * 2, 4, 2, 1, bias=False)
+            bn = BatchNorm2d(self.nfeatures * mult * 2)
+            activation = LeakyReLU(0.02, inplace=True)
+            self.layers.append(conv2d)
+            self.layers.append(bn)
+            self.layers.append(activation)
+            mult *= 2
+        conv2d = Conv2d(self.nfeatures * mult, self.nfeatures *
+                        mult * 2, 4, 1, 0, bias=False)
+        self.layers.append(conv2d)
+        self.activation = Sigmoid()
+
+        # Ensure all parameters are accessible
+        for i, layer in enumerate(self.layers):
+            self.add_module(f'layer{i}', layer)
 
     def forward(self, x):
-        if self.is_critic:
-            x = leaky_relu(self.conv1(x))
-            x = leaky_relu(self.bn1(self.conv2(x)))
-            x = leaky_relu(self.bn2(self.conv3(x)))
-            x = leaky_relu(self.bn3(self.conv4(x)))
-            x = self.fc1(x.view(-1, self.fc1_size))
-        else:
-            x = self.fc1(x).view(-1, 8 * self.nfeatures, self.final_size, self.final_size)
-            x = relu(self.bn1(self.conv1(x)))
-            x = relu(self.bn2(self.conv2(x)))
-            x = relu(self.bn3(self.conv3(x)))
-            x = torch.tanh(self.conv4(x))
+        if self.debug:
+            print("DCGAN Critic Forward Method:\n")
+        for layer in self.layers:
+            if self.debug:
+                print(layer)
+                print(x.shape, '=>', end=' ')
+            x = layer(x)
+            if self.debug:
+                print(x.shape)
+                print()
+
+        if not self.gp_enabled:
+            if self.debug:
+                print(self.activation)
+                print(x.shape, '=>', end=' ')
+            x = self.activation(x)
+            if self.debug:
+                print(x.shape)
+                print()
+
         return x
 
 
-def convolution_size(imsize, kernal_size=3, stride=2, padding=0, dilation=1):
-    """ Helper function to calculate image size after a convolution. """
-    return math.floor((imsize + 2 * padding - dilation * (kernal_size - 1) - 1) / stride + 1)
+class GeneratorArchitecture(Module):
+    ''' Standard DCGAN generator architecture (base 2 images sizes only) '''
 
+    def __init__(self, settings: dict):
+        super().__init__()
+
+
+def isbase2(x):
+    ''' Checks if x is a power of 2 '''
+    y = math.log2(x)
+    return (y - (y // 1)) == 0
+
+
+if __name__ == "__main__":
+
+    d = CriticArchitecture({
+        'gp_enabled': False,
+        'image_size': 28,
+        'nchannels': 3,
+        'nfeatures': 128
+    }, debug=True)
+
+    with torch.no_grad():
+        x = torch.randn(128, 3, 64, 64)
+        d(x)
