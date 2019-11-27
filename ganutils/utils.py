11d5c7d57cc30a0244fb12d3ecdfe1e004c1b7e9
ggolish <ggolish@sycamores.indstate.edu>
2019-11-27 02:39:19 -0500

    Added function to generate images from a gan.

--- ganutils/utils.py
+++ ganutils/utils.py
@@ -1,5 +1,18 @@
 
+import torch
 import numpy as np
+
+
+def generate_images(gan, n):
+    gan.cpu()
+    with torch.no_grad():
+        images = []
+        for i in range(n):
+            z = gan.get_latent_vec(1)
+            img = gan.generate_image(z)[0].numpy()
+            images.append(img)
+        images = np.array(images)
+        return clean_images(images)
 
 
 def clean_images(images):
