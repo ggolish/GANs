0345c0aa604fd64c4f06145b130adc96eb008088
Adam <adavenport9@sycamores.indstate.edu>
2019-12-10 16:26:07 -0500

    Adding refresh function

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from tkinter import *
+import argparse
 import numpy as np
 import artgan
 import torch
@@ -38,8 +39,48 @@ def selection(event):
     img = ImageTk.PhotoImage(Image.fromarray(images))
     canvas.itemconfig(canvas_image, image=img)
 
+def refresh(event):
+    global z
+    global canvas
+    global canvas_image
+    global images
+    global img
+    z = torch.randn(cols*rows,100,1,1).to(gan.dev)
+    with torch.no_grad():
+        images = gan.G(z).cpu().numpy()
+        images = clean_images(images)
+    images = images[:cols*rows].reshape(rows, cols, 64, 64, 3)
+    images = images.swapaxes(1, 2)
+    images = images.reshape(height * rows, width * cols, 3)
+    img = ImageTk.PhotoImage(Image.fromarray(images))
+    canvas.itemconfig(canvas_image, image=img)
+
    
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser('GAN Visualizer')
+    parser.add_argument(
+        'session',
+        type=str,
+        help='Completed training session to use for visualization.'
+    )
+
+    parser.add_argument(
+        '-r'
+        '--rows',
+        type=int,
+        default=5,
+        help='Number of rows to display.'
+    )
+
+    parser.add_argument(
+        '-c'
+        '--cols',
+        type=int,
+        default=5,
+        help='Number of columns to display.'
+    )
+
+   
     name = 'ross-2'
     rows = 10
     cols = 10
@@ -48,7 +89,7 @@ if __name__ == '__main__':
         gan = artgan.GAN(results['settings'])
         gan.D.load_state_dict(results['d_state_dict'])
         gan.G.load_state_dict(results['g_state_dict'])
-        gan.cuda()
+        # gan.cuda()
     except Exception as e:
         print(e)
     with torch.no_grad():
@@ -65,5 +106,6 @@ if __name__ == '__main__':
     img = ImageTk.PhotoImage(Image.fromarray(images))
     canvas_image = canvas.create_image(0, 0, image=img, anchor=NW)
     canvas.bind("<Button-1>", selection)
+    canvas.bind("<Button-3>", refresh)
     canvas.pack()
     root.mainloop()
